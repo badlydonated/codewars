20058f03ccb0134909c5ea9bae8998010362201e
Jef Maldonado <jmalyall@gmail.com>
2024-03-24 22:08:11 -0500

    added codewars 8kyu kata solution

---
+++ reversed-strings.js
@@ -0,0 +1,11 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+// my solution (after googling how to reverse characters in a string )
+function solution(str){
+ return str.split('').reverse().join('')
+}
+
+//codewars other solutions
+const solution = str => str.split('').reverse().join('');
