4a0edae10c3f4c8833c84ab19e69378f42edc47b
Jef Maldonado <jefmaldonado70@gmail.com>
2024-04-06 09:23:00 -0500

    adding daily codewars challenge

---
+++ Opposites-Attract.js
@@ -0,0 +1,26 @@
+// Timmy & Sarah think they are in love, but around where they live, 
+// they will only know once they pick a flower each. 
+// If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+//My attempt
+function lovefunc(flower1, flower2){
+    if(flower1 % 2 === 0 && flower2 % 2 !== 0 || flower2 % 2 === 0 && flower1 % 2 !== 0){
+        return true
+    }else return false
+}
+
+console.log(lovefunc(1,4))
+
+//codewars solutions
+function lovefunc(flower1, flower2){
+    return flower1 % 2 !== flower2 % 2;
+  }
+
+//--------------------------------
+function lovefunc(flower1, flower2){
+    return (flower1 + flower2) % 2 === 1
+  }
+
+
