6ae9ea90316d151099449194a805ccc636c8a34a
Jef Maldonado <jefmaldonado70@gmail.com>
2024-04-10 15:45:48 -0500

    adding daily codewars challenge

---
+++ squaring-an-argument.js
@@ -0,0 +1,15 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+//My attempt
+function square(num){
+    return num ** 2 
+}
+console.log(square(5))
+
+//codewars solutions
+const square = (n) => n * n;
+
+//---------------------------
+var square = function(a){
+    return a*a;
+  }
