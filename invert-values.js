0789808545b8eeca18f7ee18003897062ac4bbc9
Jef Maldonado <jefmaldonado70@gmail.com>
2024-04-18 09:55:49 -0500

    adding daily codewars challenge

---
+++ invert-values.js
@@ -0,0 +1,22 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+//My attempt
+//P: all values are integers. do not mutate array.
+//R: return a new array containing the additive inverse of each element from the original array 
+//E: see above
+//P: take in an array. loop through each element. check for pos or neg. if pos, convert to neg. add element to new array. if neg, convert to pos. add element to new array.
+
+
+function invert(array) {
+   newArr = array.map(element => element * -1)
+   return newArr 
+}
+console.log(invert([1,2,3,4,5]))
+
+
+
