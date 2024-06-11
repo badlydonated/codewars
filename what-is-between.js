3eed7076599b7cf7f8c2ab54536404328d40b8a0
Jef Maldonado <jefmaldonado70@gmail.com>
2024-06-10 21:48:31 -0500

    adding daily codewars challenge

---
+++ what-is-between.js
@@ -0,0 +1,22 @@
+// Complete the function that takes two integers (a, b, where a < b) 
+// and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+//my attempt
+function between(a, b) {
+    let arr = []
+    for(let i = a; i <= b; i++){
+        arr.push(i)
+    }
+    return arr
+  }
+
+console.log(between(-2,2))
+
+//codewars solutions
+const between = (a, b) => Array.from(new Array(b-a+1), (_, i) => a + i);
