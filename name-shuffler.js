d5692c40aae2979ab131e691c3237cba29e34d1a
Jef Maldonado <jefmaldonado70@gmail.com>
2024-07-23 16:07:35 -0500

    adding daily codewars challenge

---
+++ name-shuffler.js
@@ -0,0 +1,12 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+
+//my attempt
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+}
+
+console.log(nameShuffler('john McClane'))
