ac1da3ddf8f01823c65c15841d8eadcca5835d04
Jef Maldonado <jefmaldonado70@gmail.com>
2024-08-07 17:11:53 -0500

    adding daily codewars challenge arrd8

---
+++ return-the-missing-element.js
@@ -0,0 +1,40 @@
+// Fellow code warrior, we need your help! We seem to have lost one of our sequence elements, 
+// and we need your help to retrieve it!
+
+// Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), 
+// but one of them seems to be missing.
+
+// Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), 
+// and returns the missing element.
+
+// Examples:
+// [0, 5, 1, 3, 2, 9, 7, 6, 4] --> 8
+// [9, 2, 4, 5, 7, 0, 8, 6, 1] --> 3
+
+//my attempt
+function getMissingElement(superImportantArray){
+    let sortedArr = superImportantArray.sort((a,b) => a-b)
+    for (let i = 0; i <= sortedArr.length; i++) {
+        if (superImportantArray.indexOf(i) === -1) 
+          return i;
+      }
+}
+
+
+console.log(getMissingElement( [0,5,1,3,2,9,7,6,4])) //, 8);
+console.log(getMissingElement( [9,2,4,5,7,0,8,6,1])) //, 3);
+console.log(getMissingElement( [9,2,4,5,7,3,8,6,1])) //, 0);
+
+//codwars solutions
+function getMissingElement(superImportantArray){
+    for (i = 0; i < 10; i++) {
+      if (superImportantArray.indexOf(i) === -1) return i;
+    }
+  }
+
+//-----------------------
+function getMissingElement(superImportantArray) {
+    return superImportantArray.reduce(function (sum, value) {return sum - value;}, 45);
+  }
+
+  
