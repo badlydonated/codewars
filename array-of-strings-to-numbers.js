a967795e5a960a88b9d8d2c0839dbbe8045d1cf1
Jef Maldonado <jefmaldonado70@gmail.com>
2024-08-09 13:07:28 -0500

    adding daily codewars challenge arrd10

---
+++ array-of-strings-to-numbers.js
@@ -0,0 +1,26 @@
+// Oh no!
+// Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
+
+// You need to cast the whole array to the correct type.
+
+// Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
+
+// ie:["1", "2", "3"] to [1, 2, 3]
+
+// Note that you can receive floats as well.
+
+//my attempt
+function toNumberArray(stringarray){
+   return stringarray.map(Number)
+}
+
+console.log(toNumberArray(["1.1","2.2","3.3"]))
+
+//codewars solutions
+function toNumberArray(stringarray)
+{
+  return stringarray.map(parseFloat);
+}
+
+//-------------------------
+const toNumberArray = stringarray => stringarray.map(Number)
