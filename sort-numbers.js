f9267d7c150cc2499cf42364573f0eb25281ce3b
Jef Maldonado <jefmaldonado70@gmail.com>
2025-09-30 17:19:14 -0500

    adding codewars challenge

---
+++ sort-numbers.js
@@ -0,0 +1,39 @@
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// For example:
+
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+
+function solution(nums){
+    if(!Array.isArray(nums)){
+        return []
+    }else if (nums.length === 0){
+        return []
+    }else {
+    return nums.sort((a,b)=> a-b)
+    }
+}
+
+console.log(solution([1,2,3,10,5])) //[1,2,3,5,10])
+console.log(solution(null)) // []
+console.log(solution([])) // []
+console.log(solution([20, 2, 10])) // [2,10,20]
+console.log(solution([2, 20, 10])) //[2,10,20]
+
+// Codewars solutions
+function solution(nums){
+  return (nums || []).sort(function(a, b){
+    return a - b
+  });
+}
+
+// -----------------------------------------------
+function solution(nums){
+    return nums !== null ? nums.sort(function(a,b){return a-b}) : [];
+}
+
+// ------------------------------------------------
+function solution(nums){
+  return nums ? nums.sort(function(a, b) { return a - b;}) : [];
+}
