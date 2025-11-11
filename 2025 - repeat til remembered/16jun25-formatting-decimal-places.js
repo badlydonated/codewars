379c6f2be80d50c3b8aa7062c3d0b6534cdc7d5d
Jef Maldonado <jefmaldonado70@gmail.com>
2025-11-11 10:53:59 -0600

    adding daily codewars challenge and notes added to a previous challenge

--- 2025 - repeat til remembered/16jun25-formatting-decimal-places.js
+++ 2025 - repeat til remembered/16jun25-formatting-decimal-places.js
@@ -17,3 +17,9 @@ function twoDecimalPlaces(n) {
 console.log(twoDecimalPlaces(4.659725356)) // 4.66
 console.log(twoDecimalPlaces(173735326.3783732637948948)) // 173735326.38
 console.log(twoDecimalPlaces(4.653725356)) // 4.65
+
+// I had it right except for ensuring the result was a number
+
+// function twoDecimalPlaces(n){
+    //return Number(n.toFixed(2))
+// }
